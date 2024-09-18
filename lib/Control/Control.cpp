0b5c4f551aa282e466139f2c5434461054758c44
mikked2101 <mikkel.blomnes@gmail.com>
2024-09-18 16:31:45 +0200

    Now working motor control from uart

--- lib/Control/Control.cpp
+++ lib/Control/Control.cpp
@@ -7,18 +7,28 @@ void Control::begin(State* state) {
     Serial2.begin(115200);
     while (!Serial2) {;}
     vesc.setSerialPort(&Serial2);
+    while(!vesc.getFWversion()){
+        Serial.println("Failed to connect to VESC");    
+        delay(1000);
+    }
+    Serial.println("Motor Initialized");
 }
 
 
 void Control::update(State* state){
+    Serial.print("Updating control: ");
+
     for (int i=0; i<3; i++) {
         if (state->enable) {
-            if (state->mode) {
+            if (state->mode) { // If speed mode (0)
+                Serial.print("Speed mode, ");
                 vesc.setRPM(state->motors[i].set_speed*16*60, can_id[i]);
-            } else {
+            } else { // If current mode (1)
+                Serial.print("Current mode, ");
                 vesc.setCurrent(state->motors[i].set_speed, can_id[i]); // left
             }
             if (vesc.getVescValues(can_id[i])) {
+                Serial.println("Got values");
                 state->motors[i].speed = vesc.data.rpm/16/60;
                 state->motors[i].current = vesc.data.avgInputCurrent;
                 state->motors[i].percentage = vesc.data.dutyCycleNow;
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,56 @@
-#include <SparkFunMPU9250-DMP.h>
 
-#include "Orient.h"
-#include "Connect.h"
+//#include <SparkFunMPU9250-DMP.h>
+
+//#include "Orient.h"
+//#include "Connect.h"
 #include "Control.h"
 #include "State.h"
 #include "UART.h"
 
-MPU9250_DMP imu;
+
+
+//MPU9250_DMP imu;
 State state;
 
+
+
 void setup() {
-    orient.begin();
-    connect.begin(&state);
+    //orient.begin();
+    //connect.begin(&state);
+    Serial.begin(9600);
+    Serial.println("Startup...");
+    state.enable = true;
+    state.mode = 1; // 0 for current, 1 for speed
     control.begin(&state);
+    
 }
 
 
 void loop() {
-    orient.update(&state);
-    state.update();
-    control.update(&state);
-    if (connect.received) {
+    //orient.update(&state);
+    //state.update();
+    for(int i = 0; i < 3; i++) {
+        state.motors[i].set_speed = 2;
+         control.update(&state);
+    }
+    
+    //Serial.println(state.motors[0].set_speed);
+    for(int i = 0; i < 3; i++) {
+        state.motors[i].set_speed = 0;
+        control.update(&state);
+    }
+   
+    //Serial.println(state.motors[0].set_speed);
+    //Serial.print(vesc.getFWversion(0))
+    /*if (connect.received) {
         connect.received = false;
         // uart.send_state_as_json(&state); // when we get the updated quaternion from the kite, we send state info over serial
     }
     if (uart.receive(&state)) { // if new message is received over uart, we send out a ping to the kite sensor to indicate that we want information back
         uart.send_state_as_json(&state); // when we get the updated quaternion from the kite, we send state info over serial
         connect.sendPing();
-    }
+        
+    }*/
 }
+
+
